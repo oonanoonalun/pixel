58d7b388ac61d21bee4e881549912c908f9aed58
David Evan Krebs <davidevankrebs@gmail.com>
2018-01-29 22:29:53 -0600

    Added input support.

--- behavior.js
+++ behavior.js
@@ -1,5 +1,8 @@
 function redChannelAlgorithm(index) {
-    pixelArray[index * 4 + 0] = (
+    if (keysDown[KEY_SPACE]) pixelArray[index * 4 + 0] = (
+		255 - distanceFromIndexToIndex[centerIndex][index] * 5
+    );
+	else pixelArray[index * 4 + 0] = (
 		distanceFromIndexToIndex[centerIndex][index] * 5
     );
 }
